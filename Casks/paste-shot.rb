33672093f9d7f0428169d833241c32167c0a96b7
Badryansah Bangsawan <badryansah99@gmail.com>
2026-09-21 22:31:17 +0900

    chore: paste-shot cask 1.0.9

--- Casks/paste-shot.rb
+++ Casks/paste-shot.rb
@@ -1,6 +1,6 @@
 cask "paste-shot" do
-  version "1.0.8"
-  sha256 "c68f0a772228e9a1f5ed58b85a20cf8b20f30c960c34685d8243d7f1686b16a5"
+  version "1.0.9"
+  sha256 "abcc904f5fa75f44b58cb61d0f79285993795609487234ddcaa24ff48b4e0e36"
 
   url "https://github.com/BadryansahBangsawan/paste-shot/releases/download/v#{version}/PasteShot.app.zip"
   name "Paste Shot"
